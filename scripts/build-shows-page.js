a76ffce4f8753e463efa7d3e0798cf5dd33232b4
Sean Yang <seanyixuan@gmail.com>
2021-10-23 16:48:23 -0700

    changed for loops to forEach

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,20 +1,19 @@
 const apiKey = "4beb74cb-bea4-450b-be89-9eeb389e2102"
 const apiURL = `https://project-1-api.herokuapp.com/showdates?api_key=${apiKey}`;
-// const axios = require("axios");
 
 axios
   .get(apiURL)
   .then((result) => {
     console.log(result.data);
-    for (let i = 0; i < shows.length; i++) {
-      displayShowsTable(result.data[i]);
-    
-    }  
+
+    result.data.forEach((show => {
+      displayShowsTable(show);
+    }))
+   
   })
   .catch((error) => {
     console.log(error);
   })
-
 
 
 // Shows Container
@@ -35,46 +34,28 @@ showsContainer.appendChild(tableBody);
 
 let shows = [
     {
-    // dateHeader: "DATE",
     date: "Mon Sept 06 2021",
-    // venueHeader: "VENUE",
     place: "Ronald Lane",
-    //locationHeader: "LOCATION",
     location: "San Francisco, CA", },
     {
-    //dateHeader: "DATE",
     date: "Tue Sept 21 2021", 
-    //venueHeader: "VENUE",
     place: "Pier 3 East",
-    //locationHeader: "LOCATION",
     location: "San Francisco, CA", },
     {
-    //dateHeader: "DATE",
     date: "Fri Oct 15 2021", 
-   // venueHeader: "VENUE",
     place: "View Lounge",
- //   locationHeader: "LOCATION",
     location: "San Francisco, CA",},
     {
- //   dateHeader: "DATE",
     date: "Sat Nov 06 2021", 
- //   venueHeader: "VENUE",
     place: "Hyatt Agency",
- //   locationHeader: "LOCATION",
     location: "San Francisco, CA", },
     {
- //   dateHeader: "DATE",
     date: "Fri Nov 26 2021", 
-  //  venueHeader: "VENUE",
     place: "Moscow Center",
-  //  locationHeader: "LOCATION",
     location: "San Francisco, CA", },
     {
-  //  dateHeader: "DATE",
     date: "Wed Dec 15 2021", 
-  //  venueHeader: "VENUE",
     place: "Press Club",
-   // locationHeader: "LOCATION",
     location: "San Francisco, CA", },
 ]
 // Creating Thead Element 
@@ -95,23 +76,6 @@ for (let key of data) {
     tableRow.appendChild(text);
     }
 }
-/* Creating the Thead in Table */
-// function generateTableHead (table) {
-//     const thead = table.createElement('thead');
-//     tableBody.appendChild(thead);
-//     const tableRow = document.createElement('tr');
-
-// }
-
-// const table = document.querySelector(".rwd-table");
-// generateTableHead(table);
-
-
-// const tableHeadRow = document.createElement("tr");
-
-// const tableCellDateHead = document.createElement("th");
-// tableCellDateHead.innerText = shows[0].dateHeader;
-// tableHeadRow.appendChild(tableCellDateHead);
 
 const displayShowsTable = (obj) => {
     const tableRow = document.createElement("tr");
@@ -121,18 +85,9 @@ const displayShowsTable = (obj) => {
     tableCellDate.innerText = obj.date;
     let timeStamp = tableCellDate.innerText;
     let date = new Date(timeStamp * 1.00001);
-    // date.toLocaleString();
     let dateString = date.toString().substring(0, 15);
     tableCellDate.innerText = dateString;
-    
-    // let curr_date = date.getDate();
-    // let curr_month = date.getMonth() + 1; //Months are zero based
-    // let curr_year = date.getFullYear();
-    // console.log(curr_date + "-" + curr_month + "-" + curr_year);
-  
-    // console.log(date.getTime());
-    // console.log(date);
-    // console.log(date.getTime());
+
     tableRow.appendChild(tableCellDate);
   
     const tableCellVenue = document.createElement("td");
@@ -154,71 +109,9 @@ const displayShowsTable = (obj) => {
 
  
       tableRow.addEventListener("click", () => {
-       
-        // for (let j = 0; j < tableRows[i].cells.length; j++) {
           tableRow.classList.toggle("selected");
-        // }
       })
     }
 
   
   
-
-  /* Commented out hard-coded table rows to give room for api extraction */
-
-  // for (let i = 0; i < shows.length; i++) {
-  //   displayShowsTable(shows[i]);
-  // }  
-
-
-//     function displayShows(sw) {
-        
-//     let singleShowContainer = document.createElement("section");
-//     singleShowContainer.classList.add("shows__single");
-//     showsContainer.appendChild(singleShowContainer);
-
-//     let showDateHeader = document.createElement("h1");
-//     showDateHeader.classList.add("shows__date-header");  
-//     showDateHeader.classList.add("shows__header"); 
-//     showDateHeader.innerText = sw.dateHeader;
-//     singleShowContainer.appendChild(showDateHeader);
-
-//     let showDate = document.createElement("p");
-//     showDate.classList.add("shows__date");  
-//     showDate.classList.add("shows__description"); 
-//     showDate.innerText = sw.date;
-//     singleShowContainer.appendChild(showDate);
-
-//     let showVenueHeader = document.createElement("h1");
-//     showVenueHeader.classList.add("shows__venue-header"); 
-//     showVenueHeader.classList.add("shows__header");  
-//     showVenueHeader.innerText = sw.venueHeader;
-//     singleShowContainer.appendChild(showVenueHeader);
-
-//     let showVenue = document.createElement("p");
-//     showVenue.classList.add("shows__venue");  
-//     showVenue.classList.add("shows__description"); 
-//     showVenue.innerText = sw.venue;
-//     singleShowContainer.appendChild(showVenue);
-
-//     let showLocationHeader = document.createElement("h1");
-//     showLocationHeader.classList.add("shows__location-header");
-//     showLocationHeader.classList.add("shows__header");   
-//     showLocationHeader.innerText = sw.locationHeader;
-//     singleShowContainer.appendChild(showLocationHeader);
-
-//     let showLocation = document.createElement("p");
-//     showLocation.classList.add("shows__location");  
-//     showLocation.classList.add("shows__description"); 
-//     showLocation.innerText = sw.location;
-//     singleShowContainer.appendChild(showLocation);
-
-//     let buyTicketsButton = document.createElement("button");
-//     buyTicketsButton.classList.add("shows__button");
-//     buyTicketsButton.innerText = sw.button;
-//     singleShowContainer.appendChild(buyTicketsButton);
-// }
-
-// for (i = 0; i < shows.length; i++) {
-//     displayShows(shows[i]);
-// };
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -4,135 +4,108 @@ const apiURL = `https://project-1-api.herokuapp.com/comments?api_key=76856582-8a
 
 const commentsContainer = document.querySelector(".comments__container");
 console.log(commentsContainer); 
+      
+function displayComment(cm) {
     
-    comments = [
-      {
-        name: "Connor Walton",
-        timestamp: "02/17/2021",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-      },
-      {
-        name: "Emilie Beach",
-        timestamp: "01/09/2021",
-        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-      },
-      {
-        name: "Miles Acosta",
-        timestamp: "12/20/2020",
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough",
-      },
-    ]
 
-  
-    function displayComment(cm) {
-        
-
-        let singleCommentContainer = document.createElement("section");
-        singleCommentContainer.classList.add("comments__single");
-        commentsContainer.appendChild(singleCommentContainer);
-    
-    
-        let commentAvatar = document.createElement("figure");
-        commentAvatar.classList.add("comments__avatar");
-        singleCommentContainer.appendChild(commentAvatar);
-        
-    
-        let commentContent = document.createElement("div");
-        commentContent.classList.add("comments__content");
-        singleCommentContainer.appendChild(commentContent);
-        
-     
-        let commentContentTop = document.createElement("div");
-        commentContentTop.classList.add("comments__content-top");
-        commentContent.appendChild(commentContentTop);
-    
-    
-        let userName = document.createElement("p");
-        userName.classList.add("comments__name");  
-        userName.innerText = cm.name;
-        commentContentTop.appendChild(userName);
-    
-    
-        let userTimeStamp = document.createElement("div");
-        userTimeStamp.classList.add("comments__timestamp");
-        userTimeStamp.innerText = cm.timestamp;
-        let timeStamp = userTimeStamp.innerText;
-        let date = new Date(timeStamp * 1.00001);
-        // date.toLocaleString();
-        let dateString = date.toLocaleString().split(',')[0];
-        console.log(dateString);
-        userTimeStamp.innerText = dateString;
-        commentContentTop.appendChild(userTimeStamp);
-    
-    
-        let commentText = document.createElement("p");
-        commentText.classList.add("comments__text");
-        commentText.innerText = cm.comment;
-        commentContent.appendChild(commentText);
-
-        // creating bottom div for like and delete button 
-        let commentContentBottom = document.createElement('div');
-        commentContentBottom.classList.add('comments__content-bottom');
-        commentContent.appendChild(commentContentBottom);
-
-        // creating like button
-        let likeButton = document.createElement('button');
-        likeButton.classList.add('likebutton');
-        likeButton.innerText = 'Like';
-        commentContentBottom.appendChild(likeButton);
-
-        
-        //creating counter for like button
-        let likeCounter = document.createElement('p');
-        likeCounter.classList.add('counter');
-        likeCounter.innerText = cm.likes;
-        commentContentBottom.appendChild(likeCounter);
+let singleCommentContainer = document.createElement("section");
+singleCommentContainer.classList.add("comments__single");
+commentsContainer.appendChild(singleCommentContainer);
 
 
-         // creating delete button
-         let deleteButton = document.createElement('button');
-         deleteButton.classList.add('deletebutton');
-         deleteButton.innerText = 'Delete';
-         commentContentBottom.appendChild(deleteButton);
+let commentAvatar = document.createElement("figure");
+commentAvatar.classList.add("comments__avatar");
+singleCommentContainer.appendChild(commentAvatar);
 
-         likeButton.addEventListener('click', (event) => {
-           
-            console.log(event);
-            console.log(cm.id);
-            commentsContainer.innerHTML = "";               // this is needed to get like counter to actually increment
-            addLike(cm.id);
-         })
 
-         deleteButton.addEventListener('click', (event) => {
-          
-           console.log(event);
-           console.log(cm);
-           commentsContainer.innerHTML = "";                // this is needed to get delete to show in real time
-           deleteComment(cm.id);
-         })
-    }
+let commentContent = document.createElement("div");
+commentContent.classList.add("comments__content");
+singleCommentContainer.appendChild(commentContent);
+
+
+let commentContentTop = document.createElement("div");
+commentContentTop.classList.add("comments__content-top");
+commentContent.appendChild(commentContentTop);
+
+
+let userName = document.createElement("p");
+userName.classList.add("comments__name");  
+userName.innerText = cm.name;
+commentContentTop.appendChild(userName);
+
+
+let userTimeStamp = document.createElement("div");
+userTimeStamp.classList.add("comments__timestamp");
+userTimeStamp.innerText = cm.timestamp;
+let timeStamp = userTimeStamp.innerText;
+let date = new Date(timeStamp * 1.00001);
+let dateString = date.toLocaleString().split(',')[0];
+console.log(dateString);
+userTimeStamp.innerText = dateString;
+commentContentTop.appendChild(userTimeStamp);
+
+
+let commentText = document.createElement("p");
+commentText.classList.add("comments__text");
+commentText.innerText = cm.comment;
+commentContent.appendChild(commentText);
+
+// creating bottom div for like and delete button 
+let commentContentBottom = document.createElement('div');
+commentContentBottom.classList.add('comments__content-bottom');
+commentContent.appendChild(commentContentBottom);
+
+// creating like button
+let likeButton = document.createElement('button');
+likeButton.classList.add('likebutton');
+likeButton.innerText = 'Like';
+commentContentBottom.appendChild(likeButton);
+
+
+//creating counter for like button
+let likeCounter = document.createElement('p');
+likeCounter.classList.add('counter');
+likeCounter.innerText = cm.likes;
+commentContentBottom.appendChild(likeCounter);
+
+
+// creating delete button
+let deleteButton = document.createElement('button');
+deleteButton.classList.add('deletebutton');
+deleteButton.innerText = 'Delete';
+commentContentBottom.appendChild(deleteButton);
+
+likeButton.addEventListener('click', (event) => {
+
+  console.log(event);
+  console.log(cm.id);
+  commentsContainer.innerHTML = "";              
+  addLike(cm.id);
+})
+
+deleteButton.addEventListener('click', (event) => {
+
+  console.log(event);
+  console.log(cm);
+  commentsContainer.innerHTML = "";              
+  deleteComment(cm.id);
+})
+}
    
-function addLike (id) {
-  
+function addLike (id) { 
   axios
     .put(`https://project-1-api.herokuapp.com/comments/${id}/like?api_key=76856582-8ae7-439d-9487-8109eb13252e`)
     .then(result => {
     
-      console.log(result);
-      callingAxios();
-      // for (i = 0; i < sortedCommentArray.length; i++) {
-      //   displayComment(sortedCommentArray[i]);               
-        
-      // };
-      
-      
+    console.log(result);
+    callingAxios();
+
     })
     .error(error => {
       console.log(error);
     })
 }
 function deleteComment (id) {
-
   axios 
     .delete(`https://project-1-api.herokuapp.com/comments/${id}/?api_key=76856582-8ae7-439d-9487-8109eb13252e`)
     .then(result => {
@@ -155,134 +128,74 @@ function submitTime () {
   return `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
 }
 
-const newComment = {
-  "name": nameInput.value,
-  "timestamp": submitTime(),
-  "content": textInput.value,
-}
-// function addingComment () {
-//   commentsContainer.innerHTML = "";
-//   comments.unshift(newComment);
-//   comments.forEach(comment => {
-//     displayComment(comment);
-//   });
-// }
-// const callingAxios = async () => {
-//   await axios
-//     .get(apiURL)
-//     .then((result) => {
-     
-//       for (i = 0; i < result.data.length; i++) {
-//         displayComment(result.data[i]);               // this gets whatever we have to start and displays it
-        
-//       };
-   
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     }
-//     )
-//   }
-//   // callingAxios();     // displays it first // when you click submit it displays a second time
-  const callingAxios = async () => {
-    await axios
-      .get(apiURL)
-      .then((result) => {
-        console.log(result.data);
-        commentArray = result.data;             // putting it in an array
+
+const callingAxios = async () => {
+  await axios
+    .get(apiURL)
+    .then((result) => {
+      console.log(result.data);
+      commentArray = result.data;            
+      console.log(commentArray);
+      arrayStorage = [];
+      for (i=3; i < commentArray.length; i=3){
         console.log(commentArray);
-        arrayStorage = [];
-        for (i=3; i < commentArray.length; i=3){
-          console.log(commentArray);
-          postedComment = commentArray.pop();
-          // console.log(arrayStorage.push(postedComment));
-          arrayStorage.push(postedComment);
-        }
-        console.log(arrayStorage);
-        console.log(commentArray);
-        // console.log(commentArray.unshift(arrayStorage));
-        commentArray.unshift(arrayStorage);
-        // console.log(commentArray);
-        // console.log(commentArray.flat());
-        sortedCommentArray = commentArray.flat();
-        console.log(sortedCommentArray);              // this includes the updated likes and deleted comments as well
-
-        // displayComment(sortedCommentArray[i]);
-
-         for (i = 0; i < sortedCommentArray.length; i++) {
-          displayComment(sortedCommentArray[i]);          // why is this displaying on refresh?        
-        };
-        
-        
-        // console.log(result.data.likes);               // contains number of likes
-        // console.log(result.data.id);                  // returns the id of the clicked like comment single 
-        // let likeCounter = document.querySelectorAll('.counter');
-        // console.log(likeCounter);
-        // console.log(likeCounter.innerHTML);
-
-
-        // postedComment = commentArray.pop();    // removing and returning last element of array
-        // console.log(postedComment);
-        // console.log(commentArray.unshift(postedComment));   // adds posted comment to the beginning of array
-        // console.log(commentArray);
-
-        // for (i = 0; i < commentArray.length; i++) {
-        //   displayComment(commentArray[i]);               
-          
-        // };
-    
-     
-      })
-      .catch((error) => {
-        console.log(error);
+        postedComment = commentArray.pop();
+        arrayStorage.push(postedComment);
       }
-      )
+      console.log(arrayStorage);
+      console.log(commentArray);
+      commentArray.unshift(arrayStorage);
+      sortedCommentArray = commentArray.flat();
+      console.log(sortedCommentArray);              
+
+      sortedCommentArray.forEach(comment => {
+        displayComment(comment);                      // displaying on refresh  
+      })
+    
+          
+  })
+    .catch((error) => {
+      console.log(error);
     }
+    )
+}
 callingAxios();
 formEl.addEventListener ("submit", async (event) => {     
-    event.preventDefault();
+  event.preventDefault();
+  
+  if (nameInput.value == "") {
+    errorResponse(nameInput, true);
+    alert("Please fill in a valid name");
 
-    // const newComment = {
-    //   "name": nameInput.value,
-    //   "timestamp": submitTime(),
-    //   "comment": textInput.value,
-    // }
-   
-    if (nameInput.value == "") {
-      errorResponse(nameInput, true);
-      alert("Please fill in a valid name");
- 
-      if (textInput.value == "") {
-        errorResponse(textInput, true);
-        alert("Please enter your comment");
-      }
-    } else if (textInput.value == "") {
+    if (textInput.value == "") {
       errorResponse(textInput, true);
-
       alert("Please enter your comment");
-    } else {
-      errorResponse(nameInput, false);
-      errorResponse(textInput, false);
-      commentsContainer.innerHTML = "";       // this should get rid of the container comments upon click
-      console.log(nameInput.value);
-      await axios
-      .post(apiURL, {
-        "name": nameInput.value,
-        "comment": textInput.value
-      })
-      .then(result => {
-        console.log(result);
-        console.log(result.data);
-        callingAxios();                         
-      })
-      .catch(error => {
-        console.log(error);
-      })
- 
-      
     }
-    
-   formEl.reset();  
+  } else if (textInput.value == "") {
+    errorResponse(textInput, true);
+
+    alert("Please enter your comment");
+  } else {
+    errorResponse(nameInput, false);
+    errorResponse(textInput, false);
+    commentsContainer.innerHTML = "";       
+    console.log(nameInput.value);
+    await axios
+    .post(apiURL, {
+      "name": nameInput.value,
+      "comment": textInput.value
+    })
+    .then(result => {
+      console.log(result);
+      console.log(result.data);
+      callingAxios();                         
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+  
+  formEl.reset();  
 })
 function errorResponse (field, error) {
   if (error) {
