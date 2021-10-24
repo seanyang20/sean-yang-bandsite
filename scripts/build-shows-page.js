7fdb6e72cc87ece5a2f3beb3f26a4c4629c2bf68
Sean Yang <seanyixuan@gmail.com>
2021-10-23 18:02:23 -0700

    got shows page to display api

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,12 +1,29 @@
 const apiKey = "4beb74cb-bea4-450b-be89-9eeb389e2102"
 const apiURL = `https://project-1-api.herokuapp.com/showdates?api_key=${apiKey}`;
 
+// Creating axios get call and displaying the showstable 
 axios
   .get(apiURL)
   .then((result) => {
-    console.log(result.data);
+    console.log(result.data[0]);
+    console.log(Object.keys(result.data[0]));
+   
+      // Creating th tag for each key
+      let showsObj = Object.keys(result.data[0]);
+      console.log(showsObj);
+      for (let key of showsObj) {
+      // Adding th tag to thead element
+      if (key != "id"){
+      const tableRow = document.createElement('th');
+      tableRow.classList.add('table__header');
+      thead.appendChild(tableRow);
+      let text = document.createTextNode(key);
+      tableRow.appendChild(text);
+      }
+    }
 
     result.data.forEach((show => {
+      console.log(show);
       displayShowsTable(show);
     }))
    
@@ -14,7 +31,6 @@ axios
   .catch((error) => {
     console.log(error);
   })
-
 
 // Shows Container
 const showsContainer = document.getElementById("shows");
@@ -32,85 +48,52 @@ tableBody.classList.add("table");
 showsContainer.appendChild(tableBody);
 
 
-let shows = [
-    {
-    date: "Mon Sept 06 2021",
-    place: "Ronald Lane",
-    location: "San Francisco, CA", },
-    {
-    date: "Tue Sept 21 2021", 
-    place: "Pier 3 East",
-    location: "San Francisco, CA", },
-    {
-    date: "Fri Oct 15 2021", 
-    place: "View Lounge",
-    location: "San Francisco, CA",},
-    {
-    date: "Sat Nov 06 2021", 
-    place: "Hyatt Agency",
-    location: "San Francisco, CA", },
-    {
-    date: "Fri Nov 26 2021", 
-    place: "Moscow Center",
-    location: "San Francisco, CA", },
-    {
-    date: "Wed Dec 15 2021", 
-    place: "Press Club",
-    location: "San Francisco, CA", },
-]
 // Creating Thead Element 
 const thead = document.createElement('thead');
 thead.classList.add('table__thead');
 tableBody.appendChild(thead);
 
-// Creating th tag for each key
-let data = Object.keys(shows[0]);
-console.log(data);
-for (let key of data) {
-    // Adding th tag to thead element
-    if (key != "button"){
-    const tableRow = document.createElement('th');
-    tableRow.classList.add('table__header');
-    thead.appendChild(tableRow);
-    let text = document.createTextNode(key);
-    tableRow.appendChild(text);
-    }
-}
 
+// Creating displayShowsTable Function
 const displayShowsTable = (obj) => {
-    const tableRow = document.createElement("tr");
-    
-    const tableCellDate = document.createElement("td");
-    tableCellDate.classList.add('date')
-    tableCellDate.innerText = obj.date;
-    let timeStamp = tableCellDate.innerText;
-    let date = new Date(timeStamp * 1.00001);
-    let dateString = date.toString().substring(0, 15);
-    tableCellDate.innerText = dateString;
 
-    tableRow.appendChild(tableCellDate);
   
-    const tableCellVenue = document.createElement("td");
-    tableCellVenue.classList.add('venue');
-    tableCellVenue.innerText = obj.place;
-    tableRow.appendChild(tableCellVenue);
-
-    const tableCellLocation = document.createElement("td");
-    tableCellLocation.classList.add('location');
-    tableCellLocation.innerText = obj.location;
-    tableRow.appendChild(tableCellLocation);
-
-    const tableCellButton = document.createElement("button");
-    tableCellButton.classList.add('button');
-    tableCellButton.innerText = "BUY TICKETS";
-    tableRow.appendChild(tableCellButton);
+  // Creating each table row card 
+  const tableRow = document.createElement("tr");
   
-    tableBody.appendChild(tableRow);
+  // Creating date section in table row card 
+  const tableCellDate = document.createElement("td");
+  tableCellDate.classList.add('date')
+  tableCellDate.innerText = obj.date;
+  let timeStamp = tableCellDate.innerText;
+  let date = new Date(timeStamp * 1.00001);
+  let dateString = date.toString().substring(0, 15);
+  tableCellDate.innerText = dateString;
+  tableRow.appendChild(tableCellDate);
 
- 
-      tableRow.addEventListener("click", () => {
-          tableRow.classList.toggle("selected");
-      })
+  // Creating venue section in each table row card 
+  const tableCellVenue = document.createElement("td");
+  tableCellVenue.classList.add('venue');
+  tableCellVenue.innerText = obj.place;
+  tableRow.appendChild(tableCellVenue);
+
+  // Creating location section in each table row card 
+  const tableCellLocation = document.createElement("td");
+  tableCellLocation.classList.add('location');
+  tableCellLocation.innerText = obj.location;
+  tableRow.appendChild(tableCellLocation);
+
+  // Creating button in each table row card 
+  const tableCellButton = document.createElement("button");
+  tableCellButton.classList.add('button');
+  tableCellButton.innerText = "BUY TICKETS";
+  tableRow.appendChild(tableCellButton);
+  tableBody.appendChild(tableRow);
+
+    // Creating a dynamic click class to make color stay
+    tableRow.addEventListener("click", () => {
+        tableRow.classList.toggle("selected");
+    })
     }
 
   
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,39 +1,40 @@
 // const apiKey = "4beb74cb-bea4-450b-be89-9eeb389e2102"
 const apiURL = `https://project-1-api.herokuapp.com/comments?api_key=76856582-8ae7-439d-9487-8109eb13252e`;
 
-
+// Creating container to store all posted comments 
 const commentsContainer = document.querySelector(".comments__container");
 console.log(commentsContainer); 
-      
+    
+// Creating a displayComments Function
 function displayComment(cm) {
     
-
+// Single container to store contents of each posted comment 
 let singleCommentContainer = document.createElement("section");
 singleCommentContainer.classList.add("comments__single");
 commentsContainer.appendChild(singleCommentContainer);
 
-
+// avatar for each posted comment 
 let commentAvatar = document.createElement("figure");
 commentAvatar.classList.add("comments__avatar");
 singleCommentContainer.appendChild(commentAvatar);
 
-
+// content for each posted comment
 let commentContent = document.createElement("div");
 commentContent.classList.add("comments__content");
 singleCommentContainer.appendChild(commentContent);
 
-
+// top part of content for each posted comment to store name and timestamp
 let commentContentTop = document.createElement("div");
 commentContentTop.classList.add("comments__content-top");
 commentContent.appendChild(commentContentTop);
 
-
+// user name for each posted comment 
 let userName = document.createElement("p");
 userName.classList.add("comments__name");  
 userName.innerText = cm.name;
 commentContentTop.appendChild(userName);
 
-
+// creating timestamp 
 let userTimeStamp = document.createElement("div");
 userTimeStamp.classList.add("comments__timestamp");
 userTimeStamp.innerText = cm.timestamp;
@@ -44,7 +45,7 @@ console.log(dateString);
 userTimeStamp.innerText = dateString;
 commentContentTop.appendChild(userTimeStamp);
 
-
+// creating text comment for each posted comment
 let commentText = document.createElement("p");
 commentText.classList.add("comments__text");
 commentText.innerText = cm.comment;
@@ -75,6 +76,7 @@ deleteButton.classList.add('deletebutton');
 deleteButton.innerText = 'Delete';
 commentContentBottom.appendChild(deleteButton);
 
+// creating click functionality for like button
 likeButton.addEventListener('click', (event) => {
 
   console.log(event);
@@ -83,6 +85,7 @@ likeButton.addEventListener('click', (event) => {
   addLike(cm.id);
 })
 
+// creating delete functionality for delete button
 deleteButton.addEventListener('click', (event) => {
 
   console.log(event);
@@ -91,44 +94,48 @@ deleteButton.addEventListener('click', (event) => {
   deleteComment(cm.id);
 })
 }
-   
+
+// creating axios put call to increment number of likes 
 function addLike (id) { 
   axios
     .put(`https://project-1-api.herokuapp.com/comments/${id}/like?api_key=76856582-8ae7-439d-9487-8109eb13252e`)
     .then(result => {
     
     console.log(result);
-    callingAxios();
+    callingAxios();       // invoking get axios call to get most updated api and displaying comments 
 
     })
     .error(error => {
       console.log(error);
     })
 }
+
+// creating axios delete call to enable deletion of comment
 function deleteComment (id) {
   axios 
     .delete(`https://project-1-api.herokuapp.com/comments/${id}/?api_key=76856582-8ae7-439d-9487-8109eb13252e`)
     .then(result => {
       console.log(result.data);
-      callingAxios();
+      callingAxios();           // invoking get axios call to get most updated api and displaying comments
     })
     .error(error => {
       console.log(error);
     })
 }
 
+// Storing selected elements and storing them in variables 
 const formEl = document.getElementById("form");
 const nameInput = document.getElementById("name");
 const textInput = document.getElementById("comment-box");
 const submitButton = document.getElementById("addComment");
 console.log(formEl);
 
+// creating function to submit time 
 function submitTime () {
   const currentDate = new Date ();
   return `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
 }
-
-
+// Creating axios get call to get most updated api 
 const callingAxios = async () => {
   await axios
     .get(apiURL)
@@ -149,19 +156,18 @@ const callingAxios = async () => {
       console.log(sortedCommentArray);              
 
       sortedCommentArray.forEach(comment => {
-        displayComment(comment);                      // displaying on refresh  
-      })
-    
-          
+        displayComment(comment);                      // displays comments for the most updated api 
+      })          
   })
     .catch((error) => {
       console.log(error);
     }
     )
 }
-callingAxios();
+callingAxios();         // the first instance of getting the api and displaying comments 
+// Creating the form functionality upon clicking submit button to create a new comment 
 formEl.addEventListener ("submit", async (event) => {     
-  event.preventDefault();
+  event.preventDefault();            // prevents browser from submitting upon refresh
   
   if (nameInput.value == "") {
     errorResponse(nameInput, true);
@@ -173,12 +179,12 @@ formEl.addEventListener ("submit", async (event) => {
     }
   } else if (textInput.value == "") {
     errorResponse(textInput, true);
-
     alert("Please enter your comment");
   } else {
     errorResponse(nameInput, false);
     errorResponse(textInput, false);
-    commentsContainer.innerHTML = "";       
+     // empties the container so that comments don't duplicate when displaying comments for updated api
+    commentsContainer.innerHTML = "";         
     console.log(nameInput.value);
     await axios
     .post(apiURL, {
@@ -193,10 +199,9 @@ formEl.addEventListener ("submit", async (event) => {
     .catch(error => {
       console.log(error);
     })
-  }
-  
-  formEl.reset();  
-})
+  } 
+  formEl.reset();             // Prevents form from resetting
+})  
 function errorResponse (field, error) {
   if (error) {
     field.style.borderColor = "#D22D2D";
